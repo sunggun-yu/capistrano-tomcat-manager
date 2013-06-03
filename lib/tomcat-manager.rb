4b4c1f6ca3a5b72a7ceee1a989b37cd1e7d0df36
Sunggun Yu <sunggun.yu@gmail.com>
2013-06-03 11:54:14 -0400

    Refactoring

--- lib/tomcat-manager.rb
+++ lib/tomcat-manager.rb
@@ -1,6 +1,6 @@
 Capistrano::Configuration.instance.load do
+  desc "Capistrano Recipe for Tomcat Manager"
   namespace :tomcat_manager do
-    desc "Capistrano Recipe for Tomcat Manager"
     desc "Stop Tomcat."
     task :stop, :roles => :hosts do
       servers = find_servers_for_task(current_task)
@@ -38,9 +38,8 @@ Capistrano::Configuration.instance.load do
       servers = find_servers_for_task(current_task)
       tomcat_manager_path = get_tomcat_manager_path()
       servers.each do |srv|
-        _command = "curl --user #{manager_user}:#{manager_pw} --upload-file #{war_file} http://#{srv}:#{port}/#{tomcat_manager_path}/deploy?path=/#{context_name}&update=true"
-        result = %x[#{_command}]
-        puts "deploy : #{s} : #{result}"
+        _command = get_tomcat_manager_url_for_deploy(srv)
+        run_command(_command, "deploy", srv)
       end
     end
 
@@ -69,24 +68,40 @@ Capistrano::Configuration.instance.load do
     end
   end
 
-  def call_tomcat_manager(host_name, manager_function, path)
+  desc "Get Tomcat manager url"
+  def get_tomcat_manager_url(host_name, manager_function, path)
     if path.length > 0
       _path = "?path=/#{path}"
     end
     tomcat_manager_path = get_tomcat_manager_path()
-    _command = "curl --user #{manager_user}:#{manager_pw} http://#{host_name}:#{port}/#{tomcat_manager_path}/#{manager_function}#{_path}"
-    result = %x[#{_command}]
-    puts "#{manager_function} : #{host_name} : #{result}"
+    return "curl --user #{manager_user}:#{manager_pw} http://#{host_name}:#{port}/#{tomcat_manager_path}/#{manager_function}#{_path}"
   end
 
+  desc "Get Tomcat manager url for Deploy"
+  def get_tomcat_manager_url_for_deploy(host_name)
+    tomcat_manager_path = get_tomcat_manager_path()
+    return "curl --user #{manager_user}:#{manager_pw} --upload-file #{war_file} http://#{srv}:#{port}/#{tomcat_manager_path}/deploy?path=/#{context_name}&update=true"
+  end
+
+  desc "Call Tomcat manager url which is including path parameter"
   def call_tomcat_manager_for_app(host_name, manager_function)
-    call_tomcat_manager(host_name, manager_function, "#{context_name}")
+    _command = get_tomcat_manager_url(host_name, manager_function, "#{context_name}")
+    run_command(_command, manager_function, host_name)
   end
 
+  desc "Call Tomcat manager url which is not including path parameter"
   def call_tomcat_manager_simple(host_name, manager_function)
-    call_tomcat_manager(host_name, manager_function, "")
+    _command = get_tomcat_manager_url(host_name, manager_function, "")
+    run_command(_command, manager_function, host_name)
+  end
+  
+  desc "Command Runner"
+  def run_command(command, manager_function, hostname) 
+    result = %x[#{command}]
+    puts "#{manager_function} : #{hostname} : #{result}"
   end
 
+  desc "Get Tocmat manager context from the given version"
   def get_tomcat_manager_path
     if tomcat_version == 7
       return "manager/text"
